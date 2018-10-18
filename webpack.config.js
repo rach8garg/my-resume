95684a2d88d03b41bada516d33adb99eddc49b94
“Rachit <rachitg@juniper.net>
2018-10-18 23:24:15 +0530

    major content changes

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,10 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = function(params){
     var webpackConfig = {
-        context: path.resolve( __dirname, './src'),     // __dirname refers to the directory where this webpack.config.js lives
-        entry: './app.js',      //This can take multiple inputs in form of an array
+        context: path.resolve( __dirname, './src'),         // __dirname refers to the directory where this webpack.config.js lives
+        entry: './app.js',                                  //This can take multiple inputs in form of an array
         output: {
-            path: path.resolve(__dirname, './dist'),    // __dirname refers to the directory where this webpack.config.js lives
+            path: path.resolve(__dirname, './public'),      // __dirname refers to the directory where this webpack.config.js lives
             filename: 'app.bundle.js',
             publicPath: '/'
         },
@@ -47,8 +47,7 @@ module.exports = function(params){
                     test: /\.(gif|png|jpe?g)$/i,
                     exclude: [/node_modules/],
                     loaders: [
-                        'file-loader?name=images/hash=sha512&digest=hex&name=[hash].[ext]',
-                        'image-webpack-loader'
+                        'file-loader?name=images/[name].[ext]'
                     ]
                 }
             ]
@@ -61,12 +60,12 @@ module.exports = function(params){
         ],
         resolve: {
             modules: [path.resolve(__dirname, './src'), 'node_modules'],
-            extensions: [ '.js', '.scss']
+            extensions: ['.js', '.scss']
         },
         devServer : {
-            contentBase: path.resolve(__dirname, './'),  //specify the path from which dev server should serve the contents
-            inline :  true,      //to run out of IFRAME, default it runs in iframe
-            stats : "errors-only",    //to output only the errors, default it outputs everything and makes log huge
+            contentBase: path.resolve(__dirname, './'),     //specify the path from which dev server should serve the contents
+            inline :  true,                                 //to run out of IFRAME, default it runs in iframe
+            stats : "errors-only",                          //to output only the errors, default it outputs everything and makes log huge
             port : 8888
         }
     };
